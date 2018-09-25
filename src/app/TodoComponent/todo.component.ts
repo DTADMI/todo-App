8b18d4bb725312a6d88b8b94ecfb94e72ecdb292
DTADMI <darryl.tadmi@soat.fr>
2018-09-25 19:04:14 +0200

    TO DO APP AS SEEN ON GITHUB : JUST TO KEEP IT

---
+++ src/app/TodoComponent/todo.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Task } from './task';
+
+@Component({
+  selector: 'todo-component',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  public tasks: Task[];
+
+  constructor() {
+    this.tasks = [];
+  }
+
+  addTask(task: string) {
+    if (task === "") return;
+    this.tasks.push({
+      text: task,
+      done: false
+    });
+  }
+  removeTask(task) {
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+  }
+  toggle(task) {
+    task.done = !task.done;
+  }
+}
